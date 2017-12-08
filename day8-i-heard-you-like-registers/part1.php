79f6d5152df660f33c19b98204236fbad52c0739
ruudvdd <ruud@inventis.be>
2017-12-08 21:48:24 +0100

    solution day 8 part 1

---
+++ day8-i-heard-you-like-registers/part1.php
@@ -0,0 +1,77 @@
+<?php
+
+$rawProgram = file_get_contents(__DIR__ . '/input');
+
+// parse program
+
+$rawInstructions = explode("\n", $rawProgram);
+
+function validCondition(string $conditionRepresentation, array $registers)
+{
+    $match = preg_match('/([a-z]+)\ ([\<\>\=\!]{1,2})\ (\-?\d+)/', $conditionRepresentation, $matches);
+
+    if (!$match) {
+        throw new \Exception(sprintf('Condition could not be read: %s', $conditionRepresentation));
+    }
+
+    $leftExpression = 0;
+
+    $register = $matches[1];
+    if (isset($registers[$register])) {
+        $leftExpression = (int)$registers[$register];
+    }
+
+    $rightExpression = (int)$matches[3];
+
+    $operator = $matches[2];
+    switch ($operator) {
+        case '<':
+            return $leftExpression < $rightExpression;
+        case '<=':
+            return $leftExpression <= $rightExpression;
+        case '>':
+            return $leftExpression > $rightExpression;
+        case '>=':
+            return $leftExpression >= $rightExpression;
+        case '==':
+            return $leftExpression === $rightExpression;
+        case '!=':
+            return $leftExpression !== $rightExpression;
+        default:
+            throw new \Exception(sprintf('Could not find meaning of operator %s', $operator));
+    }
+}
+
+$registers = [];
+
+foreach ($rawInstructions as $rawInstruction) {
+
+    $match = preg_match('/^([a-z]+)\ (inc|dec)\ (\-?\d+)\ if\ (.+)$/', $rawInstruction, $matches);
+
+    if (!$match) {
+        throw new \Exception(sprintf('Instruction could not be read: %s', $rawInstruction));
+    }
+
+    $register = $matches[1];
+    $instruction = $matches[2];
+    $weight = (int)$matches[3];
+    $condition = $matches[4];
+
+
+    if (!validCondition($condition, $registers)) {
+        // skip
+        continue;
+    }
+
+    if (!isset($registers[$register])) {
+        $registers[$register] = 0;
+    }
+
+    if ($instruction === 'dec') {
+        $registers[$register] -= $weight;
+    } else {
+        $registers[$register] += $weight;
+    }
+}
+
+echo max(...array_values($registers));
