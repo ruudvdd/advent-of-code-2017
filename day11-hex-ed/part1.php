8d87fb165beeee9ec4e1642a5cd7e3b086bb6136
ruudvdd <ruud@inventis.be>
2017-12-11 19:07:46 +0100

    day 11 part 1 solution

---
+++ day11-hex-ed/part1.php
@@ -0,0 +1,32 @@
+<?php
+
+$steps = explode(',', file_get_contents(__DIR__ . '/input'));
+
+$x = 0;
+$y = 0;
+
+foreach ($steps as $step) {
+    switch ($step) {
+        case 'ne':
+            $x += 1;
+            break;
+        case 'se':
+            $x += 1;
+            $y -= 1;
+            break;
+        case 's':
+            $y -= 1;
+            break;
+        case 'sw':
+            $x -= 1;
+            break;
+        case 'nw':
+            $x -= 1;
+            $y += 1;
+            break;
+        case 'n':
+            $y += 1;
+    }
+}
+
+echo $x + $y;
